ca28f3938d19e79f8f283e29da8d4feb10a5983a
gamburgm <mitch.gamburg@gmail.com>
2022-07-27 17:43:57 -0400

    CSS support with macro

--- 7gui/counter.rkt
+++ 7gui/counter.rkt
@@ -1,5 +1,20 @@
 #lang racketscript/base
 
+(require (for-syntax syntax/parse
+                     racket/base))
+
+(define-syntax (with-css stx)
+  (syntax-parse stx
+    [(_ ([k v] ...+) body ...+)
+     #'(let ()
+         (define body-res
+           (let ()
+             body ...))
+
+         (add-style! body-res (symbol->string 'k) (symbol->string 'v))
+         ...
+         body-res)]))
+  
 (define DOM #js*.document)
 
 (define BODY-NODE #js.DOM.body)
@@ -37,9 +52,27 @@
       (update-content! display-node (unbox cnt)))
 
     (update-content! display-node 0)
-    (define btn (button-node "Click me!" (λ (_) (updater))))
+
+    (define btn
+      (with-css ([margin-left 10px])
+        (button-node "Click me!" (λ (_) (updater)))))
+
+;;     (define btn (button-node "Click me!" (λ (_) (updater))))
 
     (add-child! parent display-node)
     (add-child! parent btn)))
-  
-(create-counter! BODY-NODE)
+
+(define (add-style! node prop-name val)
+  (let ([curr-style ($ node 'style)])
+    (($ curr-style 'setProperty)
+     (js-string prop-name)
+     (or (js-string val) #js""))))
+
+(with-css ([padding 10px]
+           [display flex])
+  (create-counter! BODY-NODE)
+  BODY-NODE)
+
+;; (add-style! BODY-NODE "padding" "10px")
+;; (add-style! BODY-NODE "display" "flex")
+;; (create-counter! BODY-NODE)
